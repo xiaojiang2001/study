4e058d5dfb729e9e503497dafadf752971c2281a
yuanjl <1811905705@qq.com>
2025-03-25 08:12:11 +0000

    sort

--- 排序/demo1-2快速选择.cpp
+++ 排序/demo1-2快速选择.cpp
@@ -6,9 +6,10 @@ using namespace std;
 
 int partition(vector<int>& array, int left, int right)
 {
-    int pivotIndex = left + (right - left) / 2;
-    int pival = array[pivotIndex];
-    swap(array[pivotIndex], array[right]);
+    int pivot = (right + left) / 2;
+    int pival = array[pivot];
+
+    swap(array[pivot], array[right]);
 
     int idx = left;
     for (int i = left; i < right; i++)
@@ -24,52 +25,34 @@ int partition(vector<int>& array, int left, int right)
 
 
 // 计算奇数个数组成的数组的中位数
-int quickFind(vector<int> &array, int left, int right, int k)
+int quickSelect(vector<int> &array, int left, int right, int k)
 {
     if(left == right)   return array[left];
-    int pivotIndex = partition(array, left, right);
-    if(pivotIndex == k)
-        return array[pivotIndex];
-    else if(pivotIndex < k)
-        quickFind(array, left, pivotIndex - 1, k);
-    else
-        quickFind(array, pivotIndex + 1, right, k);
-}
+    int pivot = partition(array, left, right);
+    if(k < left || k > right)
+        return -1;
 
-// 计算一个子数组是否满足条件
-bool subArray(vector<int>& array)
-{
-    if (array.size() < 1 || array.size() % 2 == 0)
-        return false;
-    // 一定是奇个数
-    int midVal = quickFind(array, 0, array.size() -1 , array.size() / 2);     
-    int midIdx = array.size() / 2 + 1;  // 中间位置
-    if (array[midIdx] == midVal)
-        return true;
-    return false;
-}
+    int ret = 0;
 
-int countArrayRes(vector<int>& array)
-{
-    int res = 0;
-    bool flag = false;
-    for (int i = 0; i < array.size(); i++)
-    {
-        for (int j = i; j < array.size(); i++)
-        {
-            vector<int> subArray(array.begin() + i, array.begin() + j + 1);
-            flag = countArrayRes(subArray);
-            if(subArray.size() % 2 == 1 && flag) {
-                res++;
-                flag = false;
-            }
-        }
+    if(k == pivot)
+        return array[k];
+    else if(k < pivot){
+        ret = quickSelect(array, left, pivot - 1, k);
+        return ret;
     }
-    
+    else{
+        ret = quickSelect(array, pivot + 1, right, k);
+        return ret;
+    }
 }
 
 int main()
 {
+    vector<int> array{1,4,7,9,10,5,8};
+    int k = 5;
+    int ret = quickSelect(array, 0, array.size() - 1, k-1);
+
+    cout << ret << " " << endl;
 
     return 0;
 }
