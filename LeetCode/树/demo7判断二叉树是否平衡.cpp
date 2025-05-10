87b93e926a2bd81f118ffb267b3acac5cacd5f92
xiaojiang <1811905705@qq.com>
2025-05-10 04:49:47 +0000

    i0509#isBalance

--- LeetCode/树/demo7判断二叉树是否平衡.cpp
+++ LeetCode/树/demo7判断二叉树是否平衡.cpp
@@ -3,8 +3,6 @@
 
 using namespace std;
 
-
-
 struct TreeNode
 {
     int val;
@@ -12,6 +10,63 @@ struct TreeNode
     TreeNode* right;
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
+
+// 递归，不平衡返回-1
+int depth(TreeNode* node)
+{
+    if(node == nullptr)
+        return 0;
+
+    int left  = depth(node->left);
+    int right = depth(node->right);
+
+    // 下面已经不平衡了
+    if(left == -1 || right == -1)
+        return -1;
+
+    // 当前触发不平衡
+    if(abs(left - right) > 1)
+        return -1;
+    
+    return max(left, right) + 1;
+}
+
+bool getBalance(TreeNode *node, int* depth)
+{
+    if(node == nullptr) {
+        *depth = 0;
+        return true;
+    }
+
+    int left, right;            // 左右高度
+    bool leftRet  = getBalance(node->left,  &left);
+    bool rightRet = getBalance(node->right, &right);
+
+    // 已经不平衡
+    if(!leftRet || !rightRet)
+        return false;
+    if(abs(left - right) > 1)
+        return false;
+    *depth = max(left, right) + 1;
+    return true;
+}
+
+bool isBalanced(TreeNode* root) 
+{
+    if(root == nullptr)
+        return true;
+    int depth;
+    bool ret = getBalance(root, &depth);
+    return ret;
+}
+
+
+
+
+
+
+
+
 
 
 
@@ -51,7 +106,6 @@ bool getBalance(TreeNode* node, int *depth)
     }
     return false;
 }
-
 
 bool isBalanced(TreeNode* root) {
     if(root == nullptr)
